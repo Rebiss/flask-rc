6934308b0119ef777fabca77585c51251a6fe2b1
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-10-29 13:53:33 +0400

    build project

---
+++ src/view/App.js
@@ -0,0 +1,11 @@
+import '../asset/App.css';
+
+export const App =() => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Flask
+      </header>
+    </div>
+  );
+}
